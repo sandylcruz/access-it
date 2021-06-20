b59e12f6b5cfbd340decba383adc714edc794fda
Sandy Cruz <sandyleahcruz@icloud.com>
2021-06-19 17:37:33 -0700

    Create primary button

--- projects/access-it/src/Button/index.tsx
+++ projects/access-it/src/Button/index.tsx
@@ -2,34 +2,55 @@ import * as React from "react";
 
 import styled from "styled-components";
 
-const StyledButton = styled.button`
-  background-color: white;
-  color: blue;
+const StyledButton = styled.button<ButtonProps>`
+  background-color: ${({ kind }) => (kind === "basic" ? "white" : "#29614d")};
+  font-size: 20px;
+  color: ${({ kind }) => (kind === "basic" ? "#0e3431" : "white")};
+
   width: 120px;
   height: 50px;
-  border: 0;
-  border-radius: 10px;
+  border: 1px solid #165549;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: 0.3s;
 
   &:hover {
-  }
-
-  &:focus {
+    background-color: ${({ kind }) =>
+      kind === "basic" ? "#e2eeee" : "#1c4335"};
   }
 
   &:active {
+    background-color: ${({ kind }) =>
+      kind === "basic" ? "#dfecec" : "#143026"};
+  }
+
+  &:focus {
+    background-color: ${({ kind }) =>
+      kind === "basic" ? "#9ed6c3" : "#10261e"};
+
+    box-shadow: ${({ kind }) =>
+      kind === "basic" ? "#cfe3e3 0px 0px 0px 5px" : "#afd0d0 0px 0px 0px 5px"};
   }
 `;
 
 interface ButtonProps extends React.HTMLProps<HTMLButtonElement> {
   children: string;
+  kind: "basic" | "primary" | "secondary";
 }
 
-const Button = React.memo<ButtonProps>(({ children, onClick, onKeyDown }) => {
-  return (
-    <StyledButton aria-label={children} onClick={onClick} onKeyDown={onKeyDown}>
-      {children}
-    </StyledButton>
-  );
-});
+const Button = React.memo<ButtonProps>(
+  ({ children, kind, onClick, onKeyDown }) => {
+    return (
+      <StyledButton
+        aria-label={children}
+        onClick={onClick}
+        onKeyDown={onKeyDown}
+        kind={kind}
+      >
+        {children}
+      </StyledButton>
+    );
+  }
+);
 
 export default Button;
--- projects/documentation/src/App.js
+++ projects/documentation/src/App.js
@@ -1,25 +1,42 @@
 import "./App.css";
-
 import { Button } from "access-it";
+import styled from "styled-components";
+
+const DemoDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  margin-top: 100px;
+`;
+
 console.log(Button);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Button
-          onKeyDown={(event) => {
-            console.log(event);
-          }}
-          onClick={(event) => {
-            console.log(event);
-          }}
-          tabIndex="0"
-        >
-          Hello World
-        </Button>
-      </header>
-    </div>
+    <DemoDiv className="App">
+      <Button
+        onKeyDown={(event) => {
+          console.log(event);
+        }}
+        onClick={(event) => {
+          console.log(event);
+        }}
+        kind={"basic"}
+      >
+        Basic
+      </Button>
+      <Button
+        onKeyDown={(event) => {
+          console.log(event);
+        }}
+        onClick={(event) => {
+          console.log(event);
+        }}
+        kind={"secondary"}
+      >
+        Primary
+      </Button>
+    </DemoDiv>
   );
 }
 
